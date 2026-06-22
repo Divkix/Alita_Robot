e531cf39109c097c0588e4c9c561906380a58801
Divanshu Chauhan <divkix@divkix.me>
2026-06-22 17:10:46 -0500

    chore(release): set BotVersion to v2.19.6

--- alita/config/config.go
+++ alita/config/config.go
@@ -223,7 +223,7 @@ func LoadConfig() (*Config, error) {
 	cfg := &Config{
 		// Core configuration
 		BotToken:    os.Getenv("BOT_TOKEN"),
-		BotVersion:  "2.19.5",
+		BotVersion:  "2.19.6",
 		ApiServer:   os.Getenv("API_SERVER"),
 		WorkingMode: "worker",
 		Debug:       typeConvertor{str: os.Getenv("DEBUG")}.Bool(),
