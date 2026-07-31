0dae2980c2c02a99fb8c1896eb74dfd548b0597b
Divanshu Chauhan <divkix@divkix.me>
2026-07-31 17:56:44 -0500

    chore: bump version to v2.21.1

--- alita/config/config.go
+++ alita/config/config.go
@@ -242,7 +242,7 @@ func LoadConfig() (*Config, error) {
 	cfg := &Config{
 		// Core configuration
 		BotToken:    os.Getenv("BOT_TOKEN"),
-		BotVersion:  "2.21.0",
+		BotVersion:  "2.21.1",
 		ApiServer:   os.Getenv("API_SERVER"),
 		WorkingMode: "worker",
 		Debug:       typeConvertor{str: os.Getenv("DEBUG")}.Bool(),
