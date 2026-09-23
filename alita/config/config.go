90eaf008dd4a8e6e2f02ba5fbfa8975c622e0449
Mira <mira@hermes-agent.local>
2026-09-23 13:21:54 -0700

    chore(release): prepare v2.23.7 patch release

--- alita/config/config.go
+++ alita/config/config.go
@@ -233,7 +233,7 @@ func LoadConfig() (*Config, error) {
 
 	cfg := &Config{
 		BotToken:    os.Getenv("BOT_TOKEN"),
-		BotVersion:  "2.23.6",
+		BotVersion:  "2.23.7",
 		ApiServer:   os.Getenv("API_SERVER"),
 		WorkingMode: "worker",
 		Debug:       typeConvertor{str: os.Getenv("DEBUG")}.Bool(),
