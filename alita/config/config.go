4cd6a97f037bba70fd6f189538e2ea4ad5aebdbb
Divanshu Chauhan <divkix@divkix.me>
2026-09-24 18:28:27 -0500

    chore(release): prepare v2.23.8 patch release

--- alita/config/config.go
+++ alita/config/config.go
@@ -268,7 +268,7 @@ func LoadConfig() (*Config, error) {
 
 	cfg := &Config{
 		BotToken:    os.Getenv("BOT_TOKEN"),
-		BotVersion:  "2.23.7",
+		BotVersion:  "2.23.8",
 		ApiServer:   os.Getenv("API_SERVER"),
 		WorkingMode: "worker",
 		Debug:       typeConvertor{str: os.Getenv("DEBUG")}.Bool(),
